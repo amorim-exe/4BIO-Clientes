fc330dbf64639f7c58a0b01bec8fd15a71bd8bdb
joao.victor <joao.victor>
2025-10-24 13:55:51 -0300

    fixed validators

--- ClienteAPI/Utils/Validadores.cs
+++ ClienteAPI/Utils/Validadores.cs
@@ -1,8 +1,9 @@
 using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace ClienteAPI.Utils
 {
-    public static class Validadores
+    public static partial class Validadores
     {
         public static bool IsValidEmail(string email)
         {
@@ -19,6 +20,13 @@ namespace ClienteAPI.Utils
         {
             if (string.IsNullOrWhiteSpace(cpf)) 
                 return false;
+            
+            var n = new string(cpf.Where(char.IsDigit).ToArray());
+            if (n.Length != 11) 
+                return false;
+            
+            if (n.All(c => c == n[0])) 
+                return false;
 
             var numbers = new string(cpf.Where(char.IsDigit).ToArray());
             return numbers.Length == 11;
@@ -27,15 +35,33 @@ namespace ClienteAPI.Utils
         public static bool IsValidRg(string rg)
         {
             if (string.IsNullOrWhiteSpace(rg)) return false;
-            var numbers = new string(rg.Where(char.IsLetterOrDigit).ToArray());
-            return numbers.Length >= 5 && numbers.Length <= 12;
+
+            rg = new string(rg.Where(char.IsLetterOrDigit).ToArray());
+
+            if (rg.Length is < 5 or > 12)
+                return false;
+
+            var regex = RegexRg();
+            return regex.IsMatch(rg);
         }
 
         public static bool IsValidCep(string cep)
         {
             if (string.IsNullOrWhiteSpace(cep)) return false;
-            var numbers = new string(cep.Where(char.IsDigit).ToArray());
-            return numbers.Length == 8;
+
+            cep = new string(cep.Where(char.IsDigit).ToArray());
+
+            if (cep.Length != 8)
+                return false;
+
+            var regex = RegexCep();
+            return regex.IsMatch(cep);
         }
+
+        [GeneratedRegex(@"^[A-Za-z0-9]{5,12}$")]
+        private static partial Regex RegexRg();
+        
+        [GeneratedRegex(@"^\d{8}$")]
+        private static partial Regex RegexCep();
     }
 }
